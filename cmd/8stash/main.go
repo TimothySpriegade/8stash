253a86808a46d4ddc72fe8f76d737f6e2278b9c8
Timothy Spriegade <timothyspriegade@outlook.de>
2025-08-29 21:32:05 +0200

    fmt.Println call has possible Printf formatting directive

--- cmd/8stash/main.go
+++ cmd/8stash/main.go
@@ -39,7 +39,7 @@ func Init() int {
 	case "drop":
 		fmt.Println("8stash Delete")
 	default:
-		fmt.Println("Unknown operation \"%s\".\n", operation)
+		fmt.Fprintf(os.Stderr, "Unknown operation: %v\n", operation)
 		os.Exit(1)
 	}
 	return 0
