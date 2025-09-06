5a8f62dba80e63bb7d5574c8c7aad8c92a18639c
Timothy Spriegade <timothyspriegade@outlook.de>
2025-09-06 09:25:31 +0200

    refactor DeleteBranch function and split it to remove complexity, introduce fmt constant for helpservice.go #14

--- internal/gitx/delete.go
+++ internal/gitx/delete.go
@@ -10,6 +10,48 @@ import (
 	"github.com/go-git/go-git/v6/plumbing"
 )
 
+func deleteLocal(branchName string, repo *git.Repository, localRefName plumbing.ReferenceName) error {
+	fmt.Printf("trying to delete branch %s locally\n", branchName)
+
+	headRef, err := repo.Head()
+	if err != nil {
+		return fmt.Errorf("could not read HEAD: %w", err)
+	}
+	if headRef.Name() == localRefName {
+		return fmt.Errorf("cannot delete current branch %q. Please switch to another branch first", branchName)
+	}
+
+	err = repo.Storer.RemoveReference(localRefName)
+	if err != nil && !errors.Is(err, plumbing.ErrReferenceNotFound) {
+		return fmt.Errorf("failed to delete local branch %q: %w", branchName, err)
+	}
+	if err == nil {
+		fmt.Printf("Local branch '%s' was deleted successfully.", branchName)
+	} else {
+		fmt.Printf("Local branch '%s' not found or already deleted.", branchName)
+	}
+	return nil
+}
+
+func deleteRemote(branchName string, repo *git.Repository, remoteRefSpec config.RefSpec, remoteName string) error {
+	fmt.Printf("trying to delete branch %s on remote\n", branchName)
+
+	pushOptions := &git.PushOptions{
+		RemoteName: remoteName,
+		RefSpecs:   []config.RefSpec{remoteRefSpec},
+	}
+
+	fmt.Printf("Attempting to delete remote branch '%s' on '%s'...\n", branchName, remoteName)
+	var err error = nil
+	err = repo.Push(pushOptions)
+	if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+		return fmt.Errorf("failed to delete remote branch: %w\n", err)
+	}
+
+	fmt.Printf("Remote branch '%s' on '%s' deleted successfully or was not present.\n", branchName, remoteName)
+	return nil
+}
+
 func DeleteBranch(branchName string) error {
 	repo, _, _, remoteName, err := getRepoContext()
 	if err != nil {
@@ -21,45 +63,16 @@ func DeleteBranch(branchName string) error {
 	}
 
 	// Delete the local branch
-	fmt.Printf("trying to delete branch %s locally\n", branchName)
 	localRefName := plumbing.NewBranchReferenceName(branchName)
-
-	headRef, err := repo.Head()
-	if err != nil {
-		return fmt.Errorf("could not read HEAD: %w", err)
-	}
-	if headRef.Name() == localRefName {
-		return fmt.Errorf("cannot delete current branch %q. Please switch to another branch first", branchName)
-	}
-
-	err = repo.Storer.RemoveReference(localRefName)
-	if err != nil && err != plumbing.ErrReferenceNotFound {
-		return fmt.Errorf("failed to delete local branch %q: %w", branchName, err)
-	}
-	if err == nil {
-		fmt.Printf("Local branch '%s' was deleted successfully.", branchName)
-	} else {
-		fmt.Printf("Local branch '%s' not found or already deleted.", branchName)
+	if err := deleteLocal(branchName, repo, localRefName); err != nil {
+		return err
 	}
 
 	// Delete the remote branch
-	fmt.Printf("trying to delete branch %s on remote\n", branchName)
 	remoteRefSpec := config.RefSpec(":" + localRefName.String())
-
-	pushOptions := &git.PushOptions{
-		RemoteName: remoteName,
-		RefSpecs:   []config.RefSpec{remoteRefSpec},
+	if err := deleteRemote(branchName, repo, remoteRefSpec, remoteName); err != nil {
+		return err
 	}
-
-	fmt.Printf("Attempting to delete remote branch '%s' on '%s'...\n", branchName, remoteName)
-
-	err = repo.Push(pushOptions)
-
-	if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
-		return fmt.Errorf("failed to delete remote branch: %w\n", err)
-	}
-
-	fmt.Printf("Remote branch '%s' on '%s' deleted successfully or was not present.\n", branchName, remoteName)
 
 	return nil
 }
--- internal/service/helpservice.go
+++ internal/service/helpservice.go
@@ -3,17 +3,18 @@ package service
 import "fmt"
 
 const spacer = "--------------------------------------------------------------------------------------------------------"
+const formatString = "  %-20s %s\n"
 
 func PrintHelp() {
 	fmt.Println("Welcome to 8stash Help!")
 	fmt.Println(spacer)
 	fmt.Println("Available commands:")
-	fmt.Printf("  %-20s %s\n", "help", "Show this help message")
-	fmt.Printf("  %-20s %s\n", "pop <number?>", "Attempts to pop a remote stash if only one exists")
-	fmt.Printf("  %-20s %s\n", "", "If there are multiple stashes, enter a stash hash to pop your desired stash")
-	fmt.Printf("  %-20s %s\n", "push", "Pushes your current local changes to a new remote stash")
-	fmt.Printf("  %-20s %s\n", "list", "List all current stashes with their respective numbers")
-	fmt.Printf("  %-20s %s\n", "drop <number>", "Delete the remote stash with the specified number")
+	fmt.Printf(formatString, "help", "Show this help message")
+	fmt.Printf(formatString, "pop <number?>", "Attempts to pop a remote stash if only one exists")
+	fmt.Printf(formatString, "", "If there are multiple stashes, enter a stash hash to pop your desired stash")
+	fmt.Printf(formatString, "push", "Pushes your current local changes to a new remote stash")
+	fmt.Printf(formatString, "list", "List all current stashes with their respective numbers")
+	fmt.Printf(formatString, "drop <number>", "Delete the remote stash with the specified number")
 	fmt.Println(spacer)
 	fmt.Println("default behavior if no command is provided:")
 	fmt.Println("8stash will attempt to push your current local changes to a new remote stash.")
