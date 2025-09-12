44b616b19af79dffc29e29fba2592d5b81bce7c0
Timothy Spriegade <timothyspriegade@outlook.de>
2025-09-12 20:14:51 +0200

    added cleanup to help command, realized i falsely named a test file wrong and renamed it closes #21

--- internal/service/helpservice.go
+++ internal/service/helpservice.go
@@ -15,6 +15,7 @@ func PrintHelp() {
 	fmt.Printf(formatString, "push", "Pushes your current local changes to a new remote stash")
 	fmt.Printf(formatString, "list", "List all current stashes with their respective numbers")
 	fmt.Printf(formatString, "drop <number>", "Delete the remote stash with the specified number")
+	fmt.Printf(formatString, "cleanup", "Deletes all stashes older than 30 days")
 	fmt.Println(spacer)
 	fmt.Println("default behavior if no command is provided:")
 	fmt.Println("8stash will attempt to push your current local changes to a new remote stash.")
