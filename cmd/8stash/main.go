c4fcb5ec9287cda659274657b236e1b3bd4a25ec
Timothy Spriegade <timothyspriegade@outlook.de>
2025-10-06 21:35:25 +0200

    make default push not panic because of missing -m flag

--- cmd/8stash/main.go
+++ cmd/8stash/main.go
@@ -38,7 +38,12 @@ func Init() int {
 		pushCmd := flag.NewFlagSet("push", flag.ExitOnError)
 		var commitMessage string
 		pushCmd.StringVarP(&commitMessage, "message", "m", "", "Add a descriptive message to a stash")
-		pushCmd.Parse(os.Args[2:])
+
+		args := []string{}
+		if len(os.Args) > 2 {
+			args = os.Args[2:]
+		}
+		pushCmd.Parse(args)
 		return push(commitMessage)
 	case "pop":
 		return pop()
